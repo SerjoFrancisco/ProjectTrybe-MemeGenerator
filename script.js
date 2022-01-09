f5700c9346364d80d2f63865059f7a0281ae372a
Sergio Francisco <spinheiro.lf@gmail.com>
2022-01-09 15:54:46 -0300

    upload de imagem

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const text = document.getElementById('text-input');
-const insert = document.getElementById('insert-text');
 const meme = document.getElementById('meme-text');
+const imageUp = document.getElementById('meme-insert');
+const imageDiv = document.getElementById('meme-image');
+
+function imageUpload() {
+  let imgSource = URL.createObjectURL(imageUp.files[0]);
+  imageDiv.src = imgSource;
+}
 
 function botar() {
   meme.innerText = text.value;
 }
 text.oninput = botar;
+
+imageDiv.addEventListener('click', imageUpload);
