32702b6769641d1a12741dc41bbac3f96616763b
Sergio Francisco <spinheiro.lf@gmail.com>
2022-01-09 16:51:06 -0300

    adicionando templates

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const meme = document.getElementById('meme-text');
 const imageUp = document.getElementById('meme-insert');
 const imageDiv = document.getElementById('meme-image');
 const imageContainer = document.getElementById('meme-image-container');
+const template = document.getElementsByTagName('img');
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
 
 function imageUpload() {
-  const imgSource = URL.createObjectURL(imageUp.files[0]);
-  imageDiv.src = imgSource;
+  imageDiv.src = URL.createObjectURL(imageUp.files[0]);
 }
 function fireUp() {
   imageContainer.style.border = '3px dashed red';
@@ -20,11 +20,18 @@ function watering() {
 function bury() {
   imageContainer.style.border = '6px groove green';
 }
+function useTemplate() {
+  imageDiv.src = event.target.src;
+}
 function botar() {
   meme.innerText = text.value;
 }
 text.oninput = botar;
-
+function listenAll() {
+  for (let i = 1; i < template.length; i += 1) {
+    template[i].addEventListener('click', useTemplate);
+  }
+}
 fire.addEventListener('click', fireUp);
 water.addEventListener('click', watering);
 earth.addEventListener('click', bury);
